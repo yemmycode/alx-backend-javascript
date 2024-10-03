cbe5899f868bd56079f2f97820f7f8e849fb4d52
yemmycode <yemiajibade17@gmail.com>
2024-10-03 15:24:37 -0700

    task 0

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,7 @@
+// ClassRoom.js
+export default class ClassRoom {
+  // Constructor to initialize the maxStudentsSize
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize; 
+  }
+}
---
+++ 0x02-ES6_classes/README.md
@@ -0,0 +1,1 @@
+The Classses Task
